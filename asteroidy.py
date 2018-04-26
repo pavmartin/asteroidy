618adfcc6d91e9b7851bf9ee7f1e3a09a8a00327
pavmartin <pav33518@spseol.cz>
2018-04-26 14:09:51 +0200

    init commit

---
+++ asteroidy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:26:10 2018
+
+@author: pav35118
+"""
+
+import pyglet
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
