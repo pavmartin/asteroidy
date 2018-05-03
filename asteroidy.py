a274907195c11b93cc84dccdc2ffe2b5a7cffa9f
pavmartin <31956809+pavmartin@users.noreply.github.com>
2018-05-03 14:02:41 +0200

    Add files via upload
    pyglet procvičení

--- asteroidy.py
+++ asteroidy.py
@@ -7,6 +7,41 @@ Created on Thu Apr 26 13:26:10 2018
 """
 
 import pyglet
+from pyglet.window.key import MOD_CTRL, A, B, C
+
 window = pyglet.window.Window()
+
+obrazek = pyglet.image.load('pig.png')
+obrazek.anchor_x = obrazek.width // 2
+obrazek.anchor_y = obrazek.height // 2
+sprite = pyglet.sprite.Sprite(obrazek)
+
+
+def tiktak(t):
+    sprite.x = sprite.x + 10*t
+    sprite.y = sprite.y + 10*t
+
+
+@window.event
+def on_draw():
+    window.clear()
+    sprite.draw()
+    
+
+@window.event
+def on_mouse_press(x, y, button, mod):
+    if button == 1:
+        sprite.x = x
+        sprite.y = y
+    elif button == 4:
+        sprite. rotation +=10
+    
+@window.event
+def on_key_press(sym, mod):
+    if sym != A:
+        print(sym, mod)
+
+
+pyglet.clock.schedule_interval(tiktak, 1/10)
 pyglet.app.run()
 print('Hotovo!')
